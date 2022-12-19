cb7746bfdecf9d40d068e8c23bb50fbce430fac5
jeongsu kim <toki159@naver.com>
2022-12-19 18:28:23 +0900

    SQL 기초강의 4강

--- oracle/SQL 기초강의 4강.sql
+++ oracle/SQL 기초강의 4강.sql
@@ -1,16 +1,34 @@
 create table member(
-	member_id char(8) not null,		-- 아이디
-	member_name char(5) not null,	-- 회원 이름
-	member_addr varchar(45)		--주소
+	member_id varchar2(8) not null,		-- 아이디
+	member_name varchar2(10) not null,	-- 회원 이름
+	member_addr varchar2(45)		--주소
 );
 
 create table product(
-	product_name char(4) not null,	--제품 이름(기본 키)
+	product_name varchar2(20) not null,	--제품 이름(기본 키)
 	cost int not null,		-- 가격
 	make_date date,			-- 제조일자
-	company char(5),		-- 제조회사
+	company varchar2(10),		-- 제조회사
 	amount int not null		-- 남은 수량
 );
 
+drop table product;
 
+insert into member(member_id,member_name,member_addr)
+values('tess','나훈아','경기 부천시 중동');
+insert into member(member_id,member_name,member_addr)
+values('hero','임영웅','서울 은형구 중산동');
+insert into member(member_id,member_name,member_addr)
+values('iyou','아이유','인천 남구 주인동');
+insert into member(member_id,member_name,member_addr)
+values('jyp','박진영','경기 고양시 장항동');
 
+insert into product(product_name,cost,make_date,company,amount)
+values('바나나',1500,'21-07-01','델몬트',17);
+insert into product(product_name,cost,make_date,company,amount)
+values('키스',2500,'21-07-01','OB',3);
+insert into product(product_name,cost,make_date,company,amount)
+values('삼각김밥',800,'21-07-01','CJ',22);
+
+select * from member;
+select * from member where member_name = '아이유';
